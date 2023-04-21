b2528c10288250fc90909a39661ce4942351a955
Vikaash Maharaj <vikaashmaharaj@gmail.com>
2023-04-21 15:31:36 -0400

    Update app.ts to add vuetify

--- DALL-E-2_Image-Generator/src/main.ts
+++ DALL-E-2_Image-Generator/src/main.ts
@@ -4,9 +4,38 @@ import { createPinia } from 'pinia'
 import App from './App.vue'
 import router from './router'
 
+import 'vuetify/styles'
+import { createVuetify } from 'vuetify'
+import * as components from 'vuetify/components'
+import * as directives from 'vuetify/directives'
+import { aliases, mdi } from 'vuetify/iconsets/mdi-svg'
+
 import './assets/main.css'
 
+const mainTheme = {
+    dark: true,
+    colors: {
+    }
+  }
+
 const app = createApp(App)
+const vuetify = createVuetify({
+  components,
+  directives,
+  icons: {
+    defaultSet: 'mdi',
+    aliases,
+    sets: {
+      mdi,
+    }
+  },
+  theme: {
+    defaultTheme: 'mainTheme',
+    themes: {
+      mainTheme,
+    }
+  }
+})
 
 app.use(createPinia())
 app.use(router)
